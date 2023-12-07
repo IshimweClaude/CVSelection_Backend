5704fde0aea5de17892f26dbc4e48b70645d79d9
Elie Kayitare <elie.kayitare@rwandair.com>
2023-12-07 20:35:39 +0200

    changing settings

--- CVSelection_Backend/CVSelection_Backend/settings.py
+++ CVSelection_Backend/CVSelection_Backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cvselectiondb',
         'USER': 'postgres',
-        'PASSWORD': 'kayitare',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
